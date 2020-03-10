07d4b2a4f36b89c237874beba5a8fac2fe96f9f4
ravibrunsvik <ravi.brunsvik@gmail.com>
2020-03-10 10:40:29 +0100

    Added keyboard shortcuts for traversing pages with left and right arrow keys.

--- src/components/Layout/button-footer/button-footer.component.jsx
+++ src/components/Layout/button-footer/button-footer.component.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import GithubContext from "../../../context/github/githubContext";
 
 import { ButtonContainer, StyledButton } from "./button-footer.styles";
@@ -7,11 +7,34 @@ const ButtonFooter = () => {
   const githubContext = useContext(GithubContext);
   const { pageStart, pageEnd, pageUp, pageDown, repos } = githubContext;
 
+  useEffect(() => {
+    window.addEventListener("keydown", onKeyDown);
+
+    // cleanup
+    return () => window.removeEventListener("keydown", onKeyDown);
+
+    // eslint-disable-next-line
+  }, []);
+
+  // Keyboard shortcuts
+  const onKeyDown = e => {
+    if (e.key === "ArrowRight" && pageEnd !== repos.length - 1) {
+      pageUp();
+      window.scroll(0, 0);
+    }
+    if (e.key === "ArrowLeft" && pageStart !== 0) {
+      pageDown();
+      window.scroll(0, 0);
+    }
+  };
+
+  // Page up / down click event
   const onClick = e => {
     e.target.innerHTML === "Previous" ? pageDown() : pageUp();
 
     window.scroll(0, 0);
   };
+
   return (
     <ButtonContainer>
       {pageStart !== 0 && (
